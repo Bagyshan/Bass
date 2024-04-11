d385538cbab68e7810cc84eacc21804dd9aa8b61
Bagyshan <bagishan01@gmail.com>
2024-04-11 16:21:20 +0600

    anal vnutri

--- app/config.py
+++ app/config.py
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-    model_config = SettingsConfigDict(env_file=".env", extra="allow")
+    model_config = SettingsConfigDict(env_file=".env-local", extra="allow")
 
 
 
